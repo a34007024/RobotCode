48ebac8b53fa2a07db676eeb5274a4ef970a2721
a34007024 <a34007024@gmail.com>
2018-09-18 20:23:19 +0800

    beautify code

--- 【MainProject】/Mega2560_command_handler_and_servo_control/Mega2560_command_handler_and_servo_control.ino
+++ 【MainProject】/Mega2560_command_handler_and_servo_control/Mega2560_command_handler_and_servo_control.ino
@@ -6,10 +6,15 @@ String commandCache[20];
 void setup() {
   Serial1.begin(115200);//與NodeMCU的通訊橋梁
   Serial.begin(115200);//與電腦的通訊橋梁(debug階段使用)
-  Serial.println("Testing the connection with computer...");
+  Serial.println("Testing the connection with computer...");//debug用
   for (int i = 0; i < 20; i++) {
     commandCache[i] = "";//初始化指令陣列
   }
+}
+//======================================================
+void loop() {
+  communicateWithNodeMCU();//-----------與Wifi通訊區塊-----------
+  debug();
 }
 //======================================================
 /*接線說明(左側為NodeMCU,右側為Mega大陸版)
@@ -19,22 +24,3 @@ void setup() {
   TX  ->  TX1
 */
 //======================================================
-void loop() {
-  //-----------與Wifi通訊區塊-----------
-  if (Serial1.available()) { //如果Serial 1(wifi板)有送訊號出來
-    incomingByte = Serial1.read();//讀取Serial 1接收到的資料
-    if (incomingByte == '$') {
-      //use '' because the dataType of incomingByte is char
-      //don't be foolish again xD! QwQ我好傻一開始竟然用""難怪沒反應
-      //===========解析指令字串並分割============
-      for (int i = 0; i < 20; i++) {
-        if (commandCache[i] == "") { //判斷哪一個指令快取陣列為空
-          commandCache[i] = Serial1.readStringUntil('.');//將新指令加到指令快取
-          break;//離開for迴圈繼續讀取Wifi送入的資料
-        }
-      }
-      //===========解析指令字串並分割============
-    }
-  }//-----------與Wifi通訊區塊-----------
-  debug();
-}
--- 【MainProject】/Mega2560_command_handler_and_servo_control/subroutine.ino
+++ 【MainProject】/Mega2560_command_handler_and_servo_control/subroutine.ino
@@ -1,4 +1,22 @@
-void debug(){
+void communicateWithNodeMCU() {
+  if (Serial1.available()) { //如果Serial 1(wifi板)有送訊號出來
+    incomingByte = Serial1.read();//讀取Serial 1接收到的資料
+    if (incomingByte == '$') {
+      //use '' because the dataType of incomingByte is char
+      //don't be foolish again xD! QwQ我好傻一開始竟然用""難怪沒反應
+      //===========解析指令字串並分割============
+      for (int i = 0; i < 20; i++) {
+        if (commandCache[i] == "") { //判斷哪一個指令快取陣列為空
+          commandCache[i] = Serial1.readStringUntil('.');//將新指令加到指令快取
+          break;//離開for迴圈繼續讀取Wifi送入的資料
+        }
+      }
+      //===========解析指令字串並分割============
+    }
+  }
+}
+
+void debug() {
   for (int i = 0; i < 20; i++) {
     if (commandCache[i] != "") {
       Serial.println(commandCache[i]);
