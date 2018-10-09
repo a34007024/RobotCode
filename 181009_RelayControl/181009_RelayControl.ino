2b3b1e6ee371975f820563c85b8709f2f14d3f27
郵差 <a34007024@gmail.com>
2018-10-09 15:59:24 +0800

    solve power supply problem in RelayControl

--- 181009_RelayControl/181009_RelayControl.ino
+++ 181009_RelayControl/181009_RelayControl.ino
@@ -1,13 +1,12 @@
-int relayPositive = 30,relayNegative = 31,relay = 32;
+int relay = 30;
+//relayPositive & relayNegative must connect to 5V and GND
+//Otherwise the power(current) will not be enough for Relay
+//繼電器的+、-端一定要接再5V、GND上，不然數位腳輸出的電流不夠驅動繼電器
+//驅動一顆5V的Relay需約75ma
 void setup() {
-  pinMode(relayPositive,OUTPUT);
-  pinMode(relayNegative,OUTPUT);
   pinMode(relay,OUTPUT);
-  digitalWrite(relayPositive,HIGH);
-  digitalWrite(relayNegative,LOW);
   digitalWrite(relay,LOW);
 }
-
 void loop() {
   digitalWrite(relay,HIGH);
   delay(2000);
