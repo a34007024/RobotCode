36b7f0c5c983051bca825198388d9a1fd1ee28e8
郵差 <a34007024@gmail.com>
2019-01-11 22:12:18 +0800

    完成右側拳

--- 【MainProject】/Mega2560_command_handler_and_servo_control/matchBuiltInCommand.ino
+++ 【MainProject】/Mega2560_command_handler_and_servo_control/matchBuiltInCommand.ino
@@ -73,7 +73,64 @@ void matchBuiltInCommandRequest(int processedCommandNumber) {//wifi與Serial por
       resetServos();
       break;
     case 102://右側拳
-
+      for (int i = 0; i < 15; i++) {//更改馬達角度
+        if (i == 0)  servoAngle[i] = 135  ;
+        if (i == 1)  servoAngle[i] = 90   ;
+        if (i == 2)  servoAngle[i] = 90   ;
+        if (i == 3)  servoAngle[i] = 45   ;
+        if (i == 4)  servoAngle[i] = 25   ;
+        if (i == 5)  servoAngle[i] = 80   ;
+        if (i == 6)  servoAngle[i] = 110  ;
+        if (i == 7)  servoAngle[i] = 0    ;
+        if (i == 8)  servoAngle[i] = 180  ;
+        if (i == 9)  servoAngle[i] = 90   ;
+        if (i == 10) servoAngle[i] = 100  ;
+        if (i == 11) servoAngle[i] = 0    ;
+        if (i == 12) servoAngle[i] = 180  ;
+        if (i == 13) servoAngle[i] = 90   ;
+        if (i == 14) servoAngle[i] = 90   ;
+      }
+      servoMove();
+      delay(200);
+      for (int i = 0; i < 15; i++) {//更改馬達角度
+        if (i == 0)  servoAngle[i] = 135  ;
+        if (i == 1)  servoAngle[i] = 90   ;
+        if (i == 2)  servoAngle[i] = 90   ;
+        if (i == 3)  servoAngle[i] = 180  ;
+        if (i == 4)  servoAngle[i] = 25   ;
+        if (i == 5)  servoAngle[i] = 155  ;
+        if (i == 6)  servoAngle[i] = 110  ;
+        if (i == 7)  servoAngle[i] = 0    ;
+        if (i == 8)  servoAngle[i] = 180  ;
+        if (i == 9)  servoAngle[i] = 90   ;
+        if (i == 10) servoAngle[i] = 100  ;
+        if (i == 11) servoAngle[i] = 0    ;
+        if (i == 12) servoAngle[i] = 180  ;
+        if (i == 13) servoAngle[i] = 90   ;
+        if (i == 14) servoAngle[i] = 90   ;
+      }
+      servoMove();
+      delay(200);
+      for (int i = 0; i < 15; i++) {//更改馬達角度
+        if (i == 0)  servoAngle[i] = 135  ;
+        if (i == 1)  servoAngle[i] = 90   ;
+        if (i == 2)  servoAngle[i] = 90   ;
+        if (i == 3)  servoAngle[i] = 180  ;
+        if (i == 4)  servoAngle[i] = 105  ;
+        if (i == 5)  servoAngle[i] = 155  ;
+        if (i == 6)  servoAngle[i] = 110  ;
+        if (i == 7)  servoAngle[i] = 0    ;
+        if (i == 8)  servoAngle[i] = 180  ;
+        if (i == 9)  servoAngle[i] = 90   ;
+        if (i == 10) servoAngle[i] = 100  ;
+        if (i == 11) servoAngle[i] = 0    ;
+        if (i == 12) servoAngle[i] = 180  ;
+        if (i == 13) servoAngle[i] = 90   ;
+        if (i == 14) servoAngle[i] = 90   ;
+      }
+      servoMove();
+      delay(1000);
+      resetServos();
       break;
     case 201://左拳
 
