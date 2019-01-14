b2ab4a06b2b16d4f50a8b58598dda961475c1c5d
Luckypig <a34007024@gmail.com>
2019-01-14 11:12:54 +0800

    fix 右直拳

--- 【MainProject】/Mega2560_command_handler_and_servo_control/matchBuiltInCommand.ino
+++ 【MainProject】/Mega2560_command_handler_and_servo_control/matchBuiltInCommand.ino
@@ -140,25 +140,6 @@ void matchBuiltInCommandRequest(int processedCommandNumber) {//wifi與Serial por
       for (int i = 0; i < 15; i++) {//更改馬達角度
         if (i == 0)  servoAngle[i] = 135   ;
         if (i == 1)  servoAngle[i] = 90   ;
-        if (i == 2)  servoAngle[i] = 90   ;
-        if (i == 3)  servoAngle[i] = 45   ;
-        if (i == 4)  servoAngle[i] = 90   ;
-        if (i == 5)  servoAngle[i] = 90   ;
-        if (i == 6)  servoAngle[i] = 110  ;
-        if (i == 7)  servoAngle[i] = 0    ;
-        if (i == 8)  servoAngle[i] = 180  ;
-        if (i == 9)  servoAngle[i] = 90   ;
-        if (i == 10) servoAngle[i] = 100  ;
-        if (i == 11) servoAngle[i] = 0    ;
-        if (i == 12) servoAngle[i] = 180  ;
-        if (i == 13) servoAngle[i] = 90   ;
-        if (i == 14) servoAngle[i] = 90   ;
-      }
-      servoMove();
-      delay(200);
-      for (int i = 0; i < 15; i++) {//更改馬達角度
-        if (i == 0)  servoAngle[i] = 135   ;
-        if (i == 1)  servoAngle[i] = 90   ;
         if (i == 2)  servoAngle[i] = 110  ;
         if (i == 3)  servoAngle[i] = 45   ;
         if (i == 4)  servoAngle[i] = 90   ;
