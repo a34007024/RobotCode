5f64e2495f53cc140e9dfcb15bcdc81f53331afc
Luckypig <a34007024@gmail.com>
2019-01-15 08:18:17 +0800

    新增謝幕用動作

--- 【MainProject】/Mega2560_command_handler_and_servo_control/matchBuiltInCommand.ino
+++ 【MainProject】/Mega2560_command_handler_and_servo_control/matchBuiltInCommand.ino
@@ -451,10 +451,164 @@ void matchBuiltInCommandRequest(int processedCommandNumber) {//wifi與Serial por
       servoMove();
       break;
     case 701://謝幕-1
-      
+      resetServos();
+      servoMove();
+      delay(200);
+      for (int i = 0; i < 15; i++) {//更改馬達角度(謝幕開始)
+        if (i == 0)  servoAngle[i] = 135  ;
+        if (i == 1)  servoAngle[i] = 0  ;
+        if (i == 2)  servoAngle[i] = 0  ;
+        if (i == 3)  servoAngle[i] = 45  ;
+        if (i == 4)  servoAngle[i] = 180  ;
+        if (i == 5)  servoAngle[i] = 180  ;
+        if (i == 6)  servoAngle[i] = 110  ;
+        if (i == 7)  servoAngle[i] = 120  ;
+        if (i == 8)  servoAngle[i] = 70  ;
+        if (i == 9)  servoAngle[i] = 90  ;
+        if (i == 10) servoAngle[i] = 100  ;
+        if (i == 11) servoAngle[i] = 130  ;
+        if (i == 12) servoAngle[i] = 40  ;
+        if (i == 13) servoAngle[i] = 90  ;
+        if (i == 14) servoAngle[i] = 120  ;
+      }
+      servoMove();
+      delay(200);
+      for (int i = 0; i < 15; i++) {//更改馬角度(謝幕)
+        if (i == 0)  servoAngle[i] = 135  ;
+        if (i == 1)  servoAngle[i] = 60  ;
+        if (i == 2)  servoAngle[i] = 0  ;
+        if (i == 3)  servoAngle[i] = 45  ;
+        if (i == 4)  servoAngle[i] = 120  ;
+        if (i == 5)  servoAngle[i] = 180  ;
+        if (i == 6)  servoAngle[i] = 110  ;
+        if (i == 7)  servoAngle[i] = 120  ;
+        if (i == 8)  servoAngle[i] = 70  ;
+        if (i == 9)  servoAngle[i] = 90  ;
+        if (i == 10) servoAngle[i] = 100  ;
+        if (i == 11) servoAngle[i] = 130  ;
+        if (i == 12) servoAngle[i] = 40  ;
+        if (i == 13) servoAngle[i] = 90  ;
+        if (i == 14) servoAngle[i] = 150  ;
+      }
+      servoMove();
+      delay(200);
+      for (int i = 0; i < 15; i++) {//更改馬角度(謝幕)
+        if (i == 0)  servoAngle[i] = 135  ;
+        if (i == 1)  servoAngle[i] = 0  ;
+        if (i == 2)  servoAngle[i] = 0  ;
+        if (i == 3)  servoAngle[i] = 45  ;
+        if (i == 4)  servoAngle[i] = 180  ;
+        if (i == 5)  servoAngle[i] = 180  ;
+        if (i == 6)  servoAngle[i] = 110  ;
+        if (i == 7)  servoAngle[i] = 120  ;
+        if (i == 8)  servoAngle[i] = 70  ;
+        if (i == 9)  servoAngle[i] = 90  ;
+        if (i == 10) servoAngle[i] = 100  ;
+        if (i == 11) servoAngle[i] = 130  ;
+        if (i == 12) servoAngle[i] = 40  ;
+        if (i == 13) servoAngle[i] = 90  ;
+        if (i == 14) servoAngle[i] = 120  ;
+      }
+      servoMove();
+      delay(200);
+      for (int i = 0; i < 15; i++) {//更改馬角度(謝幕)
+        if (i == 0)  servoAngle[i] = 135  ;
+        if (i == 1)  servoAngle[i] = 60  ;
+        if (i == 2)  servoAngle[i] = 0  ;
+        if (i == 3)  servoAngle[i] = 45  ;
+        if (i == 4)  servoAngle[i] = 120  ;
+        if (i == 5)  servoAngle[i] = 180  ;
+        if (i == 6)  servoAngle[i] = 110  ;
+        if (i == 7)  servoAngle[i] = 120  ;
+        if (i == 8)  servoAngle[i] = 70  ;
+        if (i == 9)  servoAngle[i] = 90  ;
+        if (i == 10) servoAngle[i] = 100  ;
+        if (i == 11) servoAngle[i] = 130  ;
+        if (i == 12) servoAngle[i] = 40  ;
+        if (i == 13) servoAngle[i] = 90  ;
+        if (i == 14) servoAngle[i] = 90  ;
+      }
+      servoMove();
+      delay(200);
+      for (int i = 0; i < 15; i++) {//更改馬角度(謝幕)
+        if (i == 0)  servoAngle[i] = 135  ;
+        if (i == 1)  servoAngle[i] = 0  ;
+        if (i == 2)  servoAngle[i] = 0  ;
+        if (i == 3)  servoAngle[i] = 45  ;
+        if (i == 4)  servoAngle[i] = 180  ;
+        if (i == 5)  servoAngle[i] = 180  ;
+        if (i == 6)  servoAngle[i] = 110  ;
+        if (i == 7)  servoAngle[i] = 120  ;
+        if (i == 8)  servoAngle[i] = 70  ;
+        if (i == 9)  servoAngle[i] = 90  ;
+        if (i == 10) servoAngle[i] = 100  ;
+        if (i == 11) servoAngle[i] = 130  ;
+        if (i == 12) servoAngle[i] = 40  ;
+        if (i == 13) servoAngle[i] = 90  ;
+        if (i == 14) servoAngle[i] = 60  ;
+      }
+      servoMove();
+      delay(200);
+      for (int i = 0; i < 15; i++) {//更改馬角度(謝幕)
+        if (i == 0)  servoAngle[i] = 135  ;
+        if (i == 1)  servoAngle[i] = 60  ;
+        if (i == 2)  servoAngle[i] = 0  ;
+        if (i == 3)  servoAngle[i] = 45  ;
+        if (i == 4)  servoAngle[i] = 120  ;
+        if (i == 5)  servoAngle[i] = 180  ;
+        if (i == 6)  servoAngle[i] = 110  ;
+        if (i == 7)  servoAngle[i] = 120  ;
+        if (i == 8)  servoAngle[i] = 70  ;
+        if (i == 9)  servoAngle[i] = 90  ;
+        if (i == 10) servoAngle[i] = 100  ;
+        if (i == 11) servoAngle[i] = 130  ;
+        if (i == 12) servoAngle[i] = 40  ;
+        if (i == 13) servoAngle[i] = 90  ;
+        if (i == 14) servoAngle[i] = 30  ;
+      }
+      servoMove();
+      delay(200);
+      for (int i = 0; i < 15; i++) {//更改馬角度(謝幕)
+        if (i == 0)  servoAngle[i] = 135  ;
+        if (i == 1)  servoAngle[i] = 0  ;
+        if (i == 2)  servoAngle[i] = 0  ;
+        if (i == 3)  servoAngle[i] = 45  ;
+        if (i == 4)  servoAngle[i] = 180  ;
+        if (i == 5)  servoAngle[i] = 180  ;
+        if (i == 6)  servoAngle[i] = 110  ;
+        if (i == 7)  servoAngle[i] = 120  ;
+        if (i == 8)  servoAngle[i] = 70  ;
+        if (i == 9)  servoAngle[i] = 90  ;
+        if (i == 10) servoAngle[i] = 100  ;
+        if (i == 11) servoAngle[i] = 130  ;
+        if (i == 12) servoAngle[i] = 40  ;
+        if (i == 13) servoAngle[i] = 90  ;
+        if (i == 14) servoAngle[i] = 60  ;
+      }
+      servoMove();
+      delay(200);
+      for (int i = 0; i < 15; i++) {//更改馬角度(謝幕)
+        if (i == 0)  servoAngle[i] = 135  ;
+        if (i == 1)  servoAngle[i] = 60  ;
+        if (i == 2)  servoAngle[i] = 0  ;
+        if (i == 3)  servoAngle[i] = 45  ;
+        if (i == 4)  servoAngle[i] = 120  ;
+        if (i == 5)  servoAngle[i] = 180  ;
+        if (i == 6)  servoAngle[i] = 110  ;
+        if (i == 7)  servoAngle[i] = 120  ;
+        if (i == 8)  servoAngle[i] = 70  ;
+        if (i == 9)  servoAngle[i] = 90  ;
+        if (i == 10) servoAngle[i] = 100  ;
+        if (i == 11) servoAngle[i] = 130  ;
+        if (i == 12) servoAngle[i] = 40  ;
+        if (i == 13) servoAngle[i] = 90  ;
+        if (i == 14) servoAngle[i] = 90  ;
+      }
+      servoMove();
+      delay(200);
       break;
     case 702://謝幕-2
-      
+
       break;
     case 999://電量顯示
       //do nothing
